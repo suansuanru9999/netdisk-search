f8aec8cda9c9613dbd2b39c45af6dd2cdb1e4005
suansuanru9999 <109578224+suansuanru9999@users.noreply.github.com>
2024-07-16 12:48:24 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,6 +42,9 @@ export default defineNuxtConfig({
                     src: 'https://challenges.cloudflare.com/turnstile/v0/api.js'
                 },
                 {
+                    src: '/sw.js'
+                },
+                {
                     src: 'https://alwingulla.com/88/tag.min.js',
                     async: true,
                     'data-zone': '79639',
