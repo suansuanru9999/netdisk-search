0ef9ec594c1b742a2ff0014ff06d00c7e3beb12e
suansuanru9999 <109578224+suansuanru9999@users.noreply.github.com>
2024-10-07 15:41:17 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -59,13 +59,9 @@ onMounted(async () => {
       <img class="w-[40px] h-[40px] sm:w-[60px] sm:h-[60px]" src="@/assets/my-logo.png" alt="logo">
       <h1 class="text-[18px] sm:text-[22px] font-serif font-bold dark:text-white ">酸酸乳-网盘资源搜索</h1>
     </div>
-    <div class="flex justify-center mt-2">
-      <a href="https://good.suansuanru.fun" target="_blank" rel="noopener noreferrer" class="text-blue-600 dark:text-blue-400 underline mx-2">
-  酸酸乳入口1
-      </a>
-      <a href="https://nb.suansuanru.fun" target="_blank" rel="noopener noreferrer" class="text-blue-600 dark:text-blue-400 underline mx-2">
-  酸酸乳入口2
-      </a>
+    <div class="link-container">
+      <a href="https://www.example1.com" target="_blank" class="link-button">链接1</a>
+      <a href="https://www.example2.com" target="_blank" class="link-button">链接2</a>
     </div>
 
     <div class="max-w-[1240px] mx-auto mt-[20px]">
